7eba1420965e749de5967d1f52e0d5716ab043fd
Farzad Asgari <khufarzadasgari@gmail.com>
2025-06-13 00:40:42 +0330

    initializing for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
             <TooltipProvider>
                 <Toaster/>
                 <Sonner/>
-                <BrowserRouter>
+                <BrowserRouter basename="/prode">
                     <Routes>
                         <Route path="/" element={<Index/>}/>
                         <Route
